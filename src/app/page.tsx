fa42618668a558d1bee6240f7e0585b136a2d3fd
Ernest Sadowski <dev@ernestsadowski.com>
2025-01-19 14:36:58 +0100

    fix: @ts-expect-error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default async function Home() {
     <div className="py-24 sm:py-32">
       <div className="container mx-auto p-6 lg:px-8">
         <h1 className="text-pretty text-4xl font-semibold tracking-tight sm:text-5xl">Superheroes</h1>
+        {/* @ts-expect-error - type error */}
         <HeroGrid superheroes={superheroes.map((hero) => ({
           id: hero.id,
           name: hero.name,
